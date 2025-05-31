4ead4508ae1ea451b91d780dc4ba55b7a3c56d45
akramcodez <skcodewizard786@gmail.com>
2025-05-31 13:20:03 +0530

    theme setup, clerk setup, prisma setup, schema setup, actions completed

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,8 @@
+//todo implemet the auth callback page
+export const dynamic = 'force-dynamic';
+
+const AuthCallbackPage = () => {
+  return <div>AuthCallbackPage</div>;
+};
+
+export default AuthCallbackPage;
